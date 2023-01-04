16b4d7df4e3c5e9e8e8eac8c89035452b9d5bbc5
Daniel <113395541+dan1oo@users.noreply.github.com>
2023-01-03 18:13:35 -0600

    Update OrbitModeling.py

--- OrbitModeling.py
+++ OrbitModeling.py
@@ -1,34 +1,44 @@
 from vpython import *
 #Web VPython 3.2
 
-#Setu-p
+#Setup
 from visual import * 
 scene = display(width=600, height=600, center=vector(0,0,0), background=color.white) 
-G=6.67*10**(-11) # Gravitational Constant 
+"""
+This program models simple orbit of a mass(spacecraft, etc.) around a planet and as affected by the gravity of similarly orbiting moons.
+"""
 
 
+
+G= 6.67*10**(-11) #Gravitational Constant 
 
 #Edit Variables
 
-Prad=3.0*10**6  # Radius of planet 
+Prad=   3.0*10**6  #Radius of planet 
 Pmass = 3.0*10**24 #Mass of planet
 
-
-
-
-
+#Creating vector objects
 planet=sphere(pos=vec(0,0,0), radius=Prad, color=color.yellow) 
+ship=sphere(pos=vec(4.0*Prad,0,0), radius=Prad/3., color=color.green, make_trail=True, trail_type="points", interval=10000) 
+moon = sphere(pos = vec(25.0*Prad, 0, 0), radius = Prad/2., color = color.blue, make_trail=True, trail_type="points", interval=10000)
+moon2 = sphere(pos = vec(0,20*Prad , 0), radius = Prad/2., color = color.yellow, make_trail=True, trail_type="points", interval=10000)
+
+
+
+
 dt=0.2
 t=0.0
+
+
+#Functions of motion
 def uppos(lol):
     #update ship position and velocity
     lol.pos = lol.v*dt+lol.pos
     lol.v = lol.a*dt +lol.v 
     return lol
-#def upa2(lol, lal, lel):
-#    lol.a = -G* lal.m/(mag(lol.pos-lal.pos))**2 * (lol.pos-lal.pos)/mag(lol.pos-lal.pos)+-G* lel.m/(mag(lol.pos-lel.pos))**2 * (lol.pos-lel.pos)/mag(lol.pos-lel.pos)
-#    return lol
+
 def upa(lol, lal):
+    #update ship acceleration
     lol.a = -G* lal.m/(mag(lol.pos-lal.pos))**2 * (lol.pos-lal.pos)/mag(lol.pos-lal.pos)
     return lol.a
 
@@ -36,10 +46,8 @@ def trt(thrust, lol):
     a = thrust*lol.v/mag(lol.v)
     return a
 
-ship=sphere(pos=vec(4.0*Prad,0,0), radius=Prad/3., color=color.green, make_trail=True, trail_type="points", interval=10000) 
-moon = sphere(pos = vec(25.0*Prad, 0, 0), radius = Prad/2., color = color.blue, make_trail=True, trail_type="points", interval=10000)
-moon2 = sphere(pos = vec(0,20*Prad , 0), radius = Prad/2., color = color.yellow, make_trail=True, trail_type="points", interval=10000)
 
+#Misc object values
 planet.m= Pmass  # Mass of planet 
 moon.m = planet.m/60.0
 moon2.m = planet.m/50.
@@ -49,6 +57,8 @@ ship.m=10000  # Mass of ship
 ship.v= 3000*vec(0,1,0)  # initial velocity of ship
 thrust = 3 #m/s^2
 
+
+#Time function
 while t<500000:  #loop to do the following indented code for 50,000 s 
     rate(5000000)   #executes at 1,000,000 Hz
      
